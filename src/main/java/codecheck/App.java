5ab5f9bfe0a5cbd2deaad0101db9ec6c774ae164
tie303922 <tie303922@S1711C12619-T1.tisnt.tis.co.jp>
2018-06-06 21:11:34 +0900

    コミット

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -1,25 +1,24 @@
 package codecheck;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
+import java.net.HttpURLConnection;
 import java.net.URL;
-
-import javax.net.ssl.HttpsURLConnection;
 
 public class App {
 	public static void main(String[] args) throws IOException {
 		for (int i = 0, l = args.length; i < l; i++) {
 			String output = String.format("argv[%s]: %s", i, args[i]);
 			System.out.println(output);
-			try {
-				URL url = new
-				           URL("/api/hash");
-				HttpsURLConnection urlConn = (HttpsURLConnection) url.openConnection();
-				urlConn.setRequestMethod("GET");
-				java.io.InputStream ist = urlConn.getInputStream();
-			} catch (MalformedURLException e) {
-				System.out.println(e);
-			}
+			String apiUrl = "/api/hash";
+			URL connectUrl = new URL(apiUrl);
+			HttpURLConnection con = (HttpURLConnection)connectUrl.openConnection();
+			con.setRequestMethod("GET");
+			con.setDoOutput(true);
+			con.setInstanceFollowRedirects(true);
+
+			System.out.println("レスポンスヘッダ:");
+			System.out.println("レスポンスコード[" + con.getResponseCode() + "] " +
+			"レスポンスメッセージ[" + con.getResponseMessage() + "]");
 		}
 	}
 }
