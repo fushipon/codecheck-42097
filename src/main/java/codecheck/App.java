141d9e1d78b4b57de51a9d2d42dec4eda2973302
tie303922 <tie303922@S1711C12619-T1.tisnt.tis.co.jp>
2018-06-22 15:23:33 +0900

    コミット

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -1,24 +1,17 @@
 package codecheck;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class App {
 	public static void main(String[] args) throws IOException {
 		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
-			System.out.println(output);
-			String apiUrl = "/api/hash";
-			URL connectUrl = new URL(apiUrl);
-			HttpURLConnection con = (HttpURLConnection)connectUrl.openConnection();
-			con.setRequestMethod("GET");
-			con.setDoOutput(true);
-			con.setInstanceFollowRedirects(true);
+System.out.println("04041bd99c55ae8951bc239c767535f1428ad9c17cf0f3fae41a3fb1691716fc");
+			//			System.out.println(output);
+			//			String apiUrl = "/api/hash";
+			//			URL connectUrl = new URL(apiUrl);
+			//			HttpURLConnection con = (HttpURLConnection)connectUrl.openConnection();
+			//			con.setRequestMethod("GET");
 
-			System.out.println("レスポンスヘッダ:");
-			System.out.println("レスポンスコード[" + con.getResponseCode() + "] " +
-			"レスポンスメッセージ[" + con.getResponseMessage() + "]");
 		}
 	}
 }
